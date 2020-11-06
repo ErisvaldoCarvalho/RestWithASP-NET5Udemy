d6ad7d6049f3c79f4f2a79e19c773c76236832f8
unknown <erisvaldocarvalhosilva@gmail.com>
2020-11-06 17:34:41 -0300

    Adicionado o controle de versão

---
+++ 03_RestWithASPNETUdemy_ControleVersao/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Script/Scripts.sql
@@ -0,0 +1,9 @@
+create table person
+(
+	id bigint(20) NOT NULL AUTO_INCREMENT,
+    address varchar(150) NOT NULL,
+    first_name varchar(150) NOT NULL,
+    last_name varchar(150) NOT NULL,
+    gender varchar(150) NOT NULL,
+    PRIMARY KEY(ID)
+);
---
+++ 03_RestWithASPNETUdemy_ControleVersao/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementations.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonServiceImplementations : IPersonService
+    {
+        private MySQLContext _context;
+        public PersonServiceImplementations(MySQLContext context)
+        {
+            _context = context;
+        }
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return person;
+        }
+
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        public Person FindById(long id)
+        {
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        public Person Update(Person person)
+        {
+            if (!Exists(person.Id)) return new Person();
+             var result = FindById(person.Id);
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            var result = FindById(id);
+            if (result != null)
+            {
+                try
+                {
+                    _context.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        private bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
+        }
+    }
+}
