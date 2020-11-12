0b9faa15be1cf4a850e7f75ad883e60a008e1627
unknown <erisvaldocarvalhosilva@gmail.com>
2020-11-12 15:11:05 -0300

    Value object and custom serialization

---
+++ 07_RestWithASPNETUdemy_ValueObject/RestWithASPNETUdemy/RestWithASPNETUdemy/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+insert into person (id, address, first_name, last_name, gender)
+values
+(1, 'Endereço 1', 'Pessoa 1', 'Sobrenome 1', 'Masculino'),
+(5, 'Endereço 5', 'Pessoa 5', 'Sobrenome 5', 'Masculino'),
+(6, 'Endereço 6', 'Pessoa 6', 'Sobrenome 6', 'Masculino'),
+(7, 'Endereço 7', 'Pessoa 7', 'Sobrenome 7', 'Masculino'),
+(8, 'Endereço 8', 'Pessoa 8', 'Sobrenome 8', 'Masculino');
---
+++ 08_RestWithASPNETUdemy_CustomSerialization/RestWithASPNETUdemy/RestWithASPNETUdemy/db/dataset/V4__Populate_Table_Books.sql
@@ -0,0 +1,8 @@
+﻿
+insert into book (id, title, author, launch_date, price)
+values
+(5, 'título 5', 'nome do autor 5', '2010-12-15', 19.42),
+(1, 'título 1', 'nome do autor 1', '2010-12-15', 19.42),
+(6, 'título 6', 'nome do autor 6', '2010-12-15', 19.42),
+(7, 'título 7', 'nome do autor 7', '2010-12-15', 19.42),
+(8, 'título 8', 'nome do autor 8', '2010-12-15', 19.42);
