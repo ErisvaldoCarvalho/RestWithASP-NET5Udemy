7ce487e167dbb216ca8132f89e0b535e953b2b25
Erisvaldo Carvalho <contato@erisvaldocarvalho.com.br>
2020-11-20 18:18:48 -0300

    CORS

---
+++ 12_RestWithASPNETUdemy_CORS/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/Converter/Implementations/PersonConverter.cs
@@ -0,0 +1,52 @@
+﻿using RestWithASPNETUdemy.Data.Converter.Contract;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNETUdemy.Data.Converter.Implementations
+{
+    public class PersonConverter : IParser<PersonVO, Person>, IParser<Person, PersonVO>
+    {
+        public Person Parser(PersonVO origin)
+        {
+            if (origin == null) return null;
+
+            return new Person()
+            {
+                Id = origin.Id,
+                Andress = origin.Andress,
+                FirstName = origin.FirstName,
+                Gender = origin.Gender,
+                LastName = origin.LastName
+            };
+        }
+
+        public List<Person> Parser(List<PersonVO> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parser(item)).ToList();
+        }
+
+        public PersonVO Parser(Person origin)
+        {
+            if (origin == null) return null;
+
+            return new PersonVO()
+            {
+                Id = origin.Id,
+                Andress = origin.Andress,
+                FirstName = origin.FirstName,
+                Gender = origin.Gender,
+                LastName = origin.LastName
+            };
+        }
+
+        public List<PersonVO> Parser(List<Person> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parser(item)).ToList();
+        }
+    }
+}
---
+++ 12_RestWithASPNETUdemy_CORS/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -0,0 +1,16 @@
+﻿using RestWithASPNETUdemy.Hypermedia;
+using RestWithASPNETUdemy.Hypermedia.Abastract;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class PersonVO: ISupportHyperMedia
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Andress { get; set; }
+        public string Gender { get; set; }
+        public List<HypermediaLink> Links { get; set; } = new List<HypermediaLink>();
+    }
+}
